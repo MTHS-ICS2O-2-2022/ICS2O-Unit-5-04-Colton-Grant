24af1b0d8014a438128e41a470f2e5f24409b389
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-05-31 16:34:13 +0000

    up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: colton
+// Created on: may 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  let inputedAge = document.getElementById("age").value
+  let inputedDay = document.getElementById("day").value
+
+  if (
+    inputedDay == "Tuesday" ||
+    inputedDay == "Thursday" ||
+    (inputedAge > 12 && inputedAge < 21)
+  ) {
+    document.getElementById("answer").innerHTML =
+      "You're able to get the discount! :)"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "Sorry, you must pay for regular pricing. :("
+  }
+}
